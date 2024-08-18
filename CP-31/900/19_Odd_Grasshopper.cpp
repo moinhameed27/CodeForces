3c5c2b28222124a44dbf6c321d342f7c7ccc94ce
Moin Hameed <moinhameed27@gmail.com>
2024-08-18 18:13:41 +0500

    Odd Grasshopper

--- CP-31/900/19_Odd_Grasshopper.cpp
+++ CP-31/900/19_Odd_Grasshopper.cpp
@@ -45,3 +45,31 @@ int main()
     }
     return 0;
 }
+
+/*
+Solution:
+e.g : x = 0, n = 15
+1. If x is even,
+0    1  2   3
+_____________
+0   -1  1   4
+0   -5  1   8
+0   -9  1   12
+0   -13 1   16
+    a) After every 4 steps from 0, distance is 0.
+    b) After every 4 steps from 1, distance is -n.
+    c) After every 4 steps from 2, distance is 1.
+    d) After every 4 steps from 3, distance is n + 1.
+
+2. If x is odd,
+0   1   2    3
+_____________
+1   2   0   -3
+1   6   0   -7
+1   10  0   -11
+1   14  0   -15
+    a) After every 4 steps from 0, distance is 1.
+    b) After every 4 steps from 1, distance is n + 1.
+    c) After every 4 steps from 2, distance is 0.
+    d) After every 4 steps from 3, distance is -n.
+*/
